846198e1f8d6ac2f6eb24f9f7e4e3488fe0be700
Nathan Gaberel <nathang@theodo.co.uk>
2017-01-09 23:07:53 +0000

    Add action to update servings.

--- src/redux/reducers/recipes.js
+++ src/redux/reducers/recipes.js
@@ -1,7 +1,19 @@
+import { types } from './recipes.actions';
+
 const INITIAL_STATE = [];
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
+    case types.SET_SERVINGS:
+      return state.map((recipe, idx) => {
+        if (idx === action.index) {
+          return {
+            ...recipe,
+            [action.servingType]: Math.max(action.value, 0)
+          };
+        }
+        else return recipe;
+      });
     default:
       return state;
   }
