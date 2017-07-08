dba2477d44294e2253e7edfd8dbde4818a6ee21c
Nathan Gaberel <nathang@theodo.co.uk>
2017-07-08 23:30:36 +0100

    RSF v0.3

--- src/redux/sagas/recipes.js
+++ src/redux/sagas/recipes.js
@@ -1,19 +1,9 @@
-import { call, put, take } from 'redux-saga/effects'
 import rsf from '../rsf'
 
 import { syncRecipes } from '@actions/recipes'
 
-function * syncRecipesSaga () {
-  const channel = yield call(rsf.database.channel, 'recipes')
-
-  while (true) {
-    const { value: recipes } = yield take(channel)
-    yield put(syncRecipes(recipes))
-  }
-}
-
 export default function * recipeSaga () {
   yield [
-    syncRecipesSaga()
+    rsf.database.sync('recipes', syncRecipes)
   ]
 }
