a7b5718d0f504f879f803dfeb9f825aa7826d753
Nathan Gaberel <nathang@theodo.co.uk>
2017-10-29 17:05:50 +0000

    Sync recipes.

--- src/redux/sagas/recipes.js
+++ src/redux/sagas/recipes.js
@@ -1,9 +1,9 @@
+import { fork } from 'redux-saga/effects'
+
 import rsf from '../rsf'
 
 import { syncRecipes } from '@actions/recipes'
 
-export default function * recipeSaga () {
-  yield [
-    rsf.database.sync('recipes', syncRecipes)
-  ]
+export default function * recipesSaga () {
+  yield fork(rsf.database.sync, 'recipes', syncRecipes)
 }
