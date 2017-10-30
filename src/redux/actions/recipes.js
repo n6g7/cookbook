edf5800cd02ba6b0199aa77d79b8e1579e2b92ee
Nathan Gaberel <nathang@theodo.co.uk>
2017-10-30 22:51:33 +0000

    Add createRecipe actions.

--- src/redux/actions/recipes.js
+++ src/redux/actions/recipes.js
@@ -1,8 +1,27 @@
 export const types = {
-  SYNC_RECIPES: 'SYNC_RECIPES'
+  SYNC_RECIPES: 'SYNC_RECIPES',
+  CREATE_RECIPE: {
+    REQUEST: 'CREATE_RECIPE.REQUEST',
+    SUCCESS: 'CREATE_RECIPE.SUCCESS',
+    FAILURE: 'CREATE_RECIPE.FAILURE'
+  }
 }
 
 export const syncRecipes = recipes => ({
   type: types.SYNC_RECIPES,
   recipes
+})
+
+export const createRecipe = recipe => ({
+  type: types.CREATE_RECIPE.REQUEST,
+  recipe
+})
+
+export const createRecipeSuccess = () => ({
+  type: types.CREATE_RECIPE.SUCCESS
+})
+
+export const createRecipeFailure = error => ({
+  type: types.CREATE_RECIPE.FAILURE,
+  error
 })
