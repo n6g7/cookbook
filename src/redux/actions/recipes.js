2afef9a0daa5842293735f48cbbb004ba2f99243
Nathan Gaberel <nathang@theodo.co.uk>
2017-10-31 22:59:12 +0000

    Cleanup recipe creation action.

--- src/redux/actions/recipes.js
+++ src/redux/actions/recipes.js
@@ -1,3 +1,5 @@
+import _mapValues from 'lodash/mapValues'
+
 export const types = {
   SYNC_RECIPES: 'SYNC_RECIPES',
   CREATE_RECIPE: {
@@ -14,7 +16,13 @@ export const syncRecipes = recipes => ({
 
 export const createRecipe = recipe => ({
   type: types.CREATE_RECIPE.REQUEST,
-  recipe
+  recipe: {
+    ...recipe,
+    calories: parseInt(recipe.calories),
+    ingredients: _mapValues(recipe.ingredients, parseFloat),
+    preparationTime: parseInt(recipe.preparationTime),
+    serves: parseInt(recipe.serves)
+  }
 })
 
 export const createRecipeSuccess = () => ({
