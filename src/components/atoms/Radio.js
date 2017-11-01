d82d64ccc84b6ff0b660b8ed7a5befef2c7dbf1b
Nathan Gaberel <nathang@theodo.co.uk>
2017-11-01 22:35:55 +0000

    Add radio button.

---
+++ src/components/atoms/Radio.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+import theme from '@theme'
+import Button from './Button'
+
+class Radio extends PureComponent {
+  static propTypes = {
+    activeColour: PropTypes.oneOf(Object.keys(theme.colours)),
+    inactiveColour: PropTypes.oneOf(Object.keys(theme.colours)),
+    radioValue: PropTypes.string.isRequired
+  }
+
+  static defaultProps = {
+    activeColour: 'green',
+    inactiveColour: 'grey'
+  }
+
+  onClick = () => this.props.input.onChange(this.props.radioValue)
+
+  render () {
+    const {
+      activeColour,
+      inactiveColour,
+      input: { value },
+      meta,
+      radioValue,
+      ...props
+    } = this.props
+
+    const active = value === radioValue
+
+    return <Button
+      onClick={this.onClick}
+      colour={active ? activeColour : inactiveColour}
+      {...props}
+    />
+  }
+}
+
+export default Radio
