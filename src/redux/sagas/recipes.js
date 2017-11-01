e21c1613e969c558181291509744baa5ff0bd8f3
Nathan Gaberel <nathang@theodo.co.uk>
2017-11-01 22:43:50 +0000

    Save recipes!

--- src/redux/sagas/recipes.js
+++ src/redux/sagas/recipes.js
@@ -1,9 +1,25 @@
-import { fork } from 'redux-saga/effects'
+import { call, fork, put, takeEvery } from 'redux-saga/effects'
 
 import rsf from '../rsf'
 
-import { syncRecipes } from '@actions/recipes'
+import {
+  createRecipeSuccess,
+  createRecipeFailure,
+  syncRecipes,
+  types
+} from '@actions/recipes'
+
+function * createRecipeSaga ({ recipe }) {
+  try {
+    yield call(rsf.database.create, 'recipes', recipe)
+    yield put(createRecipeSuccess())
+  } catch (error) {
+    yield put(createRecipeFailure(error))
+  }
+}
 
 export default function * recipesSaga () {
   yield fork(rsf.database.sync, 'recipes', syncRecipes)
+
+  yield takeEvery(types.CREATE_RECIPE.REQUEST, createRecipeSaga)
 }
