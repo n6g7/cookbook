652283b18d356e58c04dd2112f818ae57aa0a908
Nathan Gaberel <nathang@theodo.co.uk>
2017-11-02 23:02:15 +0000

    Remove unused code.

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,5 +1,4 @@
 export { default as Button } from './Button'
-export { default as Counter } from './Counter'
 export { default as FileButton } from './FileButton'
 export { default as Ingredient } from './Ingredient'
 export { default as Input } from './Input'
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,2 +1,1 @@
 export { default as ImageSelector } from './ImageSelector'
-export { default as Recipe } from './Recipe'
