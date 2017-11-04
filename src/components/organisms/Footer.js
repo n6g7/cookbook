512adf3ca4c0116d7bf94674a57f7e0c2f9127c0
Nathan Gaberel <nathang@theodo.co.uk>
2017-11-04 20:17:41 +0000

    Remove useless margin on Footer.

--- src/components/organisms/Footer.js
+++ src/components/organisms/Footer.js
@@ -15,7 +15,6 @@ const ButtonRow = styled.div`
   display: flex;
   flex-flow: row nowrap;
   justify-content: center;
-  margin-bottom: ${p => 2 * p.theme.spacing}px;
 
   button {
     margin: 0 ${p => 1.5 * p.theme.spacing}px;
