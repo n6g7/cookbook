eef0ff3e006fe9f583b982573564b6fe56dfea34
Nathan Gaberel <nathang@theodo.co.uk>
2017-11-19 15:36:43 +0000

    Refactor router selectors.

--- src/redux/selectors/router.js
+++ src/redux/selectors/router.js
@@ -2,11 +2,13 @@ import { createSelector } from 'reselect'
 
 export const pathnameSelector = state => state.router.location.pathname
 
-export const recipeIdSelector = createSelector(
+const createRouterSelector = pathRegex => createSelector(
   pathnameSelector,
   pathname => {
-    const match = /^\/recipes\/([a-zA-Z0-9_-]+)(\/|$)/.exec(pathname)
+    const match = pathRegex.exec(pathname)
     if (match) return match[1]
     else return null
   }
 )
+
+export const recipeIdSelector = createRouterSelector(/^\/recipes\/([a-zA-Z0-9_-]+)(\/|$)/)
