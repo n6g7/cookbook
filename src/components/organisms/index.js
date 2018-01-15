00eefde3b0f73cf491e97150e67001d3d21f5671
Nathan Gaberel <nathang@theodo.co.uk>
2018-01-15 22:47:12 +0000

    Add basket creation form.

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,3 +1,4 @@
+export { default as AddBasketForm } from './AddBasketForm'
 export { default as AddRecipeForm } from './AddRecipeForm'
 export { default as AddRecipeIngredientsForm } from './AddRecipeIngredientsForm'
 export { default as Footer } from './Footer'
