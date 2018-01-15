1ac67ede69c4bfa1e4312523514031c12ba501be
Nathan Gaberel <nathang@theodo.co.uk>
2018-01-15 22:59:01 +0000

    Save new basket

--- src/redux/sagas/baskets.js
+++ src/redux/sagas/baskets.js
@@ -1,9 +1,16 @@
-import { fork, takeLatest } from 'redux-saga/effects'
+import { call, fork, put, select, takeEvery, takeLatest } from 'redux-saga/effects'
+import { reset } from 'redux-form'
+import { push } from 'react-router-redux'
 
 import rsf from '../rsf'
 
 import { types as authTypes } from '@actions/auth'
-import { syncBaskets } from '@actions/baskets'
+import {
+  createBasketSuccess,
+  createBasketFailure,
+  syncBaskets,
+  types
+} from '@actions/baskets'
 
 function * syncBasketsSaga ({ user }) {
   if (!user) return
@@ -17,6 +24,20 @@ function * syncBasketsSaga ({ user }) {
   )
 }
 
+function * createBasketSaga ({ basket }) {
+  const uid = yield select(state => state.auth.user.uid)
+
+  try {
+    yield call(rsf.database.create, `baskets/${uid}`, basket)
+    yield put(createBasketSuccess())
+    yield put(reset('createBasket'))
+    yield put(push('/baskets'))
+  } catch (error) {
+    yield put(createBasketFailure(error))
+  }
+}
+
 export default function * basketsSaga () {
   yield takeLatest(authTypes.SYNC_USER, syncBasketsSaga)
+  yield takeEvery(types.CREATE_BASKET.REQUEST, createBasketSaga)
 }
