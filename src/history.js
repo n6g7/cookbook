b21ce0aa28bf08c387b7f8adc74ed961e6092fc7
Nathan Gaberel <nathang@theodo.co.uk>
2018-01-18 22:31:10 +0000

    Properly set basename.

--- src/history.js
+++ src/history.js
@@ -1,5 +1,13 @@
 import createHistory from 'history/createBrowserHistory'
 
-const history = createHistory()
+function parseUrl (url) {
+  const link = document.createElement('a')
+  link.href = url
+  return link
+}
+
+const basename = parseUrl(document.baseURI).pathname
+
+const history = createHistory({ basename })
 
 export default history
