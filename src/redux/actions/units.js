6a5f8657c44a62ab0a8c5fa3ee99a5be8a0fe7bc
Nathan Gaberel <nathang@theodo.co.uk>
2018-01-20 14:43:39 +0000

    Add createUnit actions + saga.

--- src/redux/actions/units.js
+++ src/redux/actions/units.js
@@ -1,8 +1,27 @@
 export const types = {
-  SYNC_UNITS: 'SYNC_UNITS'
+  SYNC_UNITS: 'SYNC_UNITS',
+  CREATE_UNIT: {
+    REQUEST: 'CREATE_UNIT.REQUEST',
+    SUCCESS: 'CREATE_UNIT.SUCCESS',
+    FAILURE: 'CREATE_UNIT.FAILURE'
+  }
 }
 
 export const syncUnits = units => ({
   type: types.SYNC_UNITS,
   units
+})
+
+export const createUnit = (unit) => ({
+  type: types.CREATE_UNIT.REQUEST,
+  unit
+})
+
+export const createUnitSuccess = () => ({
+  type: types.CREATE_UNIT.SUCCESS
+})
+
+export const createUnitFailure = error => ({
+  type: types.CREATE_UNIT.FAILURE,
+  error
 })
--- src/redux/sagas/units.js
+++ src/redux/sagas/units.js
@@ -1,8 +1,24 @@
-import { fork } from 'redux-saga/effects'
+import { call, fork, put, takeEvery } from 'redux-saga/effects'
+import { push } from 'react-router-redux'
 
 import rsf from '../rsf'
 
-import { syncUnits } from '@actions/units'
+import {
+  createUnitSuccess,
+  createUnitFailure,
+  syncUnits,
+  types
+} from '@actions/units'
+
+function * createUnitSaga ({ unit }) {
+  try {
+    yield call(rsf.database.create, 'units', unit)
+    yield put(createUnitSuccess())
+    yield put(push('/units'))
+  } catch (error) {
+    yield put(createUnitFailure(error))
+  }
+}
 
 export default function * unitsSaga () {
   yield fork(
@@ -12,4 +28,5 @@ export default function * unitsSaga () {
       successActionCreator: syncUnits
     }
   )
+  yield takeEvery(types.CREATE_UNIT.REQUEST, createUnitSaga)
 }
