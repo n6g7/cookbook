f6d4ce01e33551cd2ec4e280a0b245eb97c39827
Nathan Gaberel <nathang@theodo.co.uk>
2018-04-02 23:17:47 +0100

    Fix redux-saga warning.

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,4 +1,4 @@
-import { fork } from 'redux-saga/effects'
+import { all, fork } from 'redux-saga/effects'
 import { saga as auth } from 'rsf-auth'
 import rsf, { authProvider } from '../rsf'
 
@@ -9,12 +9,12 @@ import recipes from './recipes'
 import units from './units'
 
 export default function * rootSaga () {
-  yield [
+  yield all([
     fork(auth, rsf, authProvider),
     fork(baskets),
     fork(categories),
     fork(ingredients),
     fork(recipes),
     fork(units)
-  ]
+  ])
 }
