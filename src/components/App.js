ba09ae428e26b415392eb022d649a9acfc4562df
Nathan Gaberel <nathang@theodo.co.uk>
2018-04-15 13:28:01 +0100

    Fix linting.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Redirect, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import styled from 'styled-components'
 
 import {
